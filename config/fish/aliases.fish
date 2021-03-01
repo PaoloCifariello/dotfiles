f72daef91e7fc333142ddbf0289f014dd0fa97af
Paolo Cifariello <paolocifa@gmail.com>
2021-03-01 22:22:44 +0100

    Exclude all but selected config folders

---
+++ config/fish/aliases.fish
@@ -0,0 +1,10 @@
+
+abbr -a l 'exa -l'
+abbr -a ls 'exa'
+abbr -a ll 'exa -la'
+
+abbr -a vi 'nvim'
+abbr -a vim 'nvim'
+
+abbr -a lg 'lazygit'
+
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,14 +1,8 @@
 
+source (dirname (status -f))/aliases.fish
+
 set PATH $HOME/.cargo/bin $PATH
 set EDITOR nvim
 
 starship init fish | source
 
-abbr -a l 'exa -l'
-abbr -a ls 'exa'
-abbr -a ll 'exa -la'
-
-abbr -a vi 'nvim'
-abbr -a vim 'nvim'
-
-abbr -a lg 'lazygit'
