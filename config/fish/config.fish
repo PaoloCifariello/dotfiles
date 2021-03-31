28c916ca0dc57a37f0fa08a56c3cfdd6d109d7c2
Paolo Cifariello <paolocifa@gmail.com>
2021-04-01 00:06:00 +0200

    Using base-16 theme in fish, added zoxide

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,5 +4,13 @@ source (dirname (status -f))/aliases.fish
 set PATH $HOME/.cargo/bin $PATH
 set EDITOR nvim
 
-starship init fish | source
+# Base16 Shell
+if status --is-interactive
+    set BASE16_SHELL "$HOME/.config/base16-shell/"
+    source "$BASE16_SHELL/profile_helper.fish"
+end
 
+# enable starship
+starship init fish | source
+# enable zoxide
+zoxide init fish | source
